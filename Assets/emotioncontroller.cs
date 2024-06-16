6c3f5e2fe88a0867523e1820749409420816001f
hacker <dev@caramella.kr>
2024-06-16 23:25:35 +0900

    feat: Emoticon Changer

--- Assets/emotioncontroller.cs
+++ Assets/emotioncontroller.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class emotioncontroller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Image emotionImage;
     void Start()
     {
+        emotionImage = this.gameObject.GetComponent<Image>();
         
+    }
+
+    void getEmotionImage()
+    {
+        var spriteImage = Resources.Load<Sprite>("Emotion/Icons/emotion_sad") as Sprite;
+        emotionImage.sprite = spriteImage;
+        Debug.Log("Emotion Image!!");
+        Debug.Log(spriteImage);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        getEmotionImage();
     }
 }
